8ce66f8068717d07ac788d40e9f459b62cbfc1c2
mohamadmasmas <masmasmohamad@gmail.com>
2024-08-19 15:29:09 -0400

    Finished test

--- tests/test_models.py
+++ tests/test_models.py
@@ -27,7 +27,7 @@ import os
 import logging
 import unittest
 from decimal import Decimal
-from service.models import Product, Category, db
+from service.models import Product, Category, db, DataValidationError
 from service import app
 from tests.factories import ProductFactory
 
@@ -136,6 +136,17 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(products[0].id, original_id)
         self.assertEqual(products[0].description, "testing")
 
+    def test_update_a_product_no_id(self):
+        """It should raise an Error"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        # Change it and save it
+        product.description = "testing"
+        product.id = None
+        self.assertRaises(Exception, product.update)
+
     def test_delete_a_product(self):
         """It should Delete a Product"""
         product = ProductFactory()
@@ -192,3 +203,42 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.category, category)
+
+    def test_find_by_price(self):
+        """It should Find Products by price"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        price = products[0].price
+        count = len([product for product in products if product.price == price])
+        found = Product.find_by_price(price)
+        found_str = Product.find_by_price(str(price))
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.price, price)
+        for product in found_str:
+            self.assertEqual(product.price, price)
+
+    def test_deserialize_1(self):
+        """It deserialize Product"""
+        product = ProductFactory()
+        mydata = product.serialize()
+        self.assertEqual(mydata["name"], product.name)
+        mydata["price"] = "None"
+        self.assertRaises(Exception, lambda: product.deserialize(mydata))
+
+    def test_deserialize_2(self):
+        """It deserialize Product"""
+        product = ProductFactory()
+        mydata = product.serialize()
+        self.assertEqual(mydata["name"], product.name)
+        mydata["category"] = None
+        self.assertRaises(DataValidationError, lambda: product.deserialize(mydata))
+
+    def test_deserialize_3(self):
+        """It deserialize Product"""
+        product = ProductFactory()
+        mydata = product.serialize()
+        self.assertEqual(mydata["name"], product.name)
+        mydata["available"] = None
+        self.assertRaises(DataValidationError, lambda: product.deserialize(mydata))
